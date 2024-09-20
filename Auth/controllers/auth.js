3acddbe2816f20841a81d28bb953ca2f244a6d0c
Anindya Manna <anindyamanna2013@gmail.com>
2024-09-20 10:12:11 +0530

    Add login user verification

--- Auth/controllers/auth.js
+++ Auth/controllers/auth.js
@@ -1,5 +1,9 @@
 const User = require("../models/user");  
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { options } = require("../routes/user");
+
+require("dotenv").config();
 
 exports.signup = async (req, res ) => {
     try {
@@ -40,4 +44,73 @@ exports.signup = async (req, res ) => {
             message: 'User cannot be registerd, please try again later'
         });
     }
+}
+
+
+exports.login = async (req, res) => {
+     try {
+        
+        //data fetch...
+        const {email, password} = req.body;
+
+        //validation on email and password..
+
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message:'Pleasefill all the details',
+            });
+        }
+
+        //check for register user.....
+        const user = await User.findOne({email});
+        if (!user) {
+            return res.status(401).json({
+                success:false,
+                messsage:'User is not registered',
+            });
+        }
+
+        const payload = {
+            email:user.email,
+            id:user._id,
+            role:user.role
+        };
+        //verify password and generate a jwt token..
+        if (await bcrypt.compare(password, user.password)) {
+            //password matched...
+            let token = jwt.sign(payload, process.env.JWT_SECRET, {
+                                    expiresIn:"2h",
+            });
+
+            user.token = token;
+            user.password = undefined;
+
+            const options = {
+                expires: new Date( Date.now() + 3 * 24 * 60 * 60 * 1000),
+                httpOnly: true,
+            }   
+
+            res.cookie("token", token, options).status(200).json({
+                success:true,
+                token,
+                user,
+                message: 'User loggedin successfully'
+            })
+
+
+        } else {
+            return res.status(403).json({
+                success:false,
+                message:'password incorrect',
+            });
+        }
+
+     } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:'Login failure',
+        });
+     }
 }
