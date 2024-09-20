c8b53ec4da1977ad534afa5f6139c3d1a306c7d3
Anindya Manna <anindyamanna2013@gmail.com>
2024-09-20 20:13:19 +0530

    Add authorization and cookies.

--- Auth/index.js
+++ Auth/index.js
@@ -4,6 +4,12 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 
+//cookie parser..............
+
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+
 app.use(express.json());
 
 const dbConnect = require("./config/database");
--- Auth/routes/user.js
+++ Auth/routes/user.js
@@ -2,8 +2,27 @@ const express = require("express");
 const router = express.Router();
 
 const {login, signup} = require("../controllers/auth");
+const {auth, isStudent, isAdmin} = require("../middleware/auth")
 
 router.post("/login", login);
 router.post("/signup", signup);
 
+//Protected route............
+
+router.get("/student", auth, isStudent, (req, res) => {
+    res.json({
+        success:true,
+        message:'Welcome to the Protected route for students'
+    });
+})
+
+router.get("/admin", auth, isAdmin, (req, res) => {
+    res.json({
+        success:true,
+        message:'Welcome to the Protected route for admin'
+    });
+})
+
+
 module.exports = router;
+
